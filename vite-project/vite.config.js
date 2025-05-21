b90d295df392429b3f221b9d13d336b871c05eea
amanpreet1998-frontend <amandhaliwal1693@gmail.com>
2025-05-21 17:50:45 +0530

    Comprehensive updates for Netlify deployment

--- vite-project/vite.config.js
+++ vite-project/vite.config.js
@@ -1,8 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
-  base: '/Portfolio-new/',
   plugins: [react()],
+  test: {
+    globals: true,
+    environment: 'jsdom',
+  }
 })
 
